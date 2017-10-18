dd49adf2f76c4523bb69beb42402023a4e14d313
Gloria Sara Panturu <gloriasarapanturu@gmail.com>
2017-10-18 18:53:26 +0200

    Pyramid without Divide

--- pyramid.c
+++ pyramid.c
@@ -29,41 +29,49 @@ int main(int argc, char* argv[])
 
   struct BigInt big_int;
   struct BigInt big_result;
-  strtobig_int(input, length, &big_int);
-  print_big_int(&big_int);
+  big_int.digits_count = strtobig_int(input, length, &big_int);
 
-   int count = 2;
+  big_result.digits_count =big_int.digits_count;
+  int count = 2;
   while (count <= 9) {
+    print_big_int(&big_int);
+    printf(" * %d = ", count);
     multiply(&big_int, count, &big_result);
     print_big_int(&big_result);
+    printf("\n");
     copy_big_int(&big_result, &big_int);
     count++;
   }
-  while (count >=2) {
+  copy_big_int(&big_result, &big_int);
+  count = 2;
+  while (count <=9) {
+    print_big_int(&big_int);
+    printf(" / %d = ", count);
     divide(&big_int, count, &big_result);
     print_big_int(&big_result);
+    printf("\n");
     copy_big_int(&big_result, &big_int);
-    count--;
+    count++;
   }
   return 0;
 }
 
 void print_big_int (const struct BigInt *big_int)
 {
-  for (int i = big_int->digits_count; i >=0 ; i--) {
-    printf("%d",big_int->the_int[i-1]);
+  for (int i = big_int->digits_count-1; i >=0 ; i--) {
+    printf("%d",big_int->the_int[i]); //Warum geht [i-1]nicht, wenn i=big_int->digits_count-1?
   }
 }
 int strtobig_int(const char *string, int length, struct BigInt *big_int)
 {
-  int digits_count;
-  for (int i = 0;i < length; i++)
+  int digits_count = 0;
+  length--;
+  for (int i = 0;i < length+1; i++)
   {
-     if (string[i] >= '0' && string[i] <= '9')
-      {
+     if (string[i] >= '0' && string[i] <= '9'){
        big_int->the_int[length-i] = string[i]-'0';
        digits_count ++;
-    }
+      }
     else{
       return i;
         }
@@ -73,43 +81,39 @@ int strtobig_int(const char *string, int length, struct BigInt *big_int)
 
 void multiply( const struct BigInt * big_int, int factor, struct BigInt * big_result)
 {
-  int product;
-  int carry;
+  int product= 1;
+  int carry = 0;
  for (int i = 0; i < big_int->digits_count ; i++)
  {
-   product = big_int -> the_int[i] * factor;
-   if (product > 9)
-   {
-     carry = product/10;
-     big_result->the_int[i] = product % 10 + carry;
-   }
-   else
-   {
-     carry = product/10;
-     big_result->the_int[i] = product + carry;
-   }
- }
+   product = big_int -> the_int[i] * factor +carry;
+   carry = product/10;
+   big_result->the_int[i] = product % 10;
+}
+  if (carry >0) {
+    big_result->digits_count++;
+    int index = big_result->digits_count-1;
+    big_result->the_int[index] = carry;
+  }
 }
 
 void divide(const struct BigInt * big_int, int divisor, struct BigInt *  big_result)
 {
-  int quotient;
-  int rest;
-  rest = 0;
+  int quotient= 0;
+  int rest = 0;
   char number_to_divide;
  for (int i = big_int->digits_count; i >0; i--)
  {
    number_to_divide = big_int->the_int[i];
 
-   while (number_to_divide < divisor && i == big_int->digits_count) {
+   while ((int)number_to_divide < divisor && i == big_int->digits_count) {
      number_to_divide += big_int->the_int[i];
 }
    if (i != big_int->digits_count ){
      rest = rest + (char)big_int->the_int[i];
    }
-   quotient = number_to_divide / divisor;
+   quotient = (int)number_to_divide / divisor;
    big_result ->the_int[i] = quotient;
-   rest = number_to_divide % divisor;
+   rest = (int)number_to_divide % divisor;
   }
 }
 
